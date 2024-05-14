36f9e6d295496bff6543b89e49b415a8e12f3058
phihc0500 <phihc0500@vietbank.com.vn>
2024-05-14 16:53:57 +0700

    feat-complet lab13

---
+++ sources/keeptrack/docs/note.md
@@ -0,0 +1,19 @@
+compelling [kəmˈpel.ɪŋ]: thuyết phục
+stuck [stʌk] : bế tắc, mắc kẹt
+Eventually [ɪˈven.tʃu.ə.li]: sau cùng
+By that time: đến lúc đó
+comprehensive [ˌkɑːm.prəˈhen.sɪv]: toàn diện
+evolved [ɪˈvɑːlvd]: phát triển
+contradict  [ˌkɑːn.trəˈdɪkt]: mâu thuẫn
+attempt [əˈtempt]: nỗ lực
+contrast [ˈkɑːn.træst]: ngược lại
+change over time: thay đổi theo thời gian
+dealing [ˈdiː.lɪŋ]: xử lý
+demonstration [ˌdem.ənˈstreɪ.ʃən] lời giảng, bài giảng
+Enthusiasm [ɪnˈθuː.zi.æz.əm]: hăng hái, nhiệt tình
+verbose [vɚˈboʊs]: dài dòng
+strives [straɪv] nỗ lực
+make sense: có ý nghĩa
+syntactic [sɪnˈtæk.tɪk] : cú pháp
+imitation [ˌɪm.əˈteɪ.ʃən] giả, bắt trước
+
---
+++ sources/keeptrack/src/projects/ProjectList.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Project } from "./Project";
+import { ProjectCard } from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
+
+
+interface ProjectListProps {
+    projects: Project[],
+    onSave: (project: Project) => void
+}
+
+export const ProjectList = ({ projects, onSave }: ProjectListProps) => {
+    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
+    const handleClick = (project: Project) => {
+        setProjectBeingEdited(project);
+    }
+
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
+    };
+
+    return (
+        <div className="row">
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    {
+                        project === projectBeingEdited ?
+                            <ProjectForm onCancel={cancelEditing} onSave={onSave}></ProjectForm> :
+                            <ProjectCard project={project} onEdit={handleClick}></ProjectCard>
+                    }
+                </div>
+            ))}
+        </div>
+    );
+}
